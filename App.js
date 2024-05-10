e3a34f97c35ec0f06879d4817e8bb84c7ed4caec
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-10 00:19:35 +0000

    Initial

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.head}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  head: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  body: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
