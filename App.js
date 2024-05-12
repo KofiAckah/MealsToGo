4b8b42a6684634a2172086ff3060c198e9793998
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-12 13:56:12 +0000

    Added Screen to the app

--- App.js
+++ App.js
@@ -1,36 +1,11 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsSceen } from "./src/features/restaurants/screen/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.head}>
-          <Text>Search</Text>
-          {/* <Searchbar placeholder="Search" /> */}
-        </View>
-        <View style={styles.body}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsSceen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  head: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  body: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const RestaurantInfo = ({ restaurant }) => {
+  return (
+    <View>
+      <Text>restaurant-info.component</Text>
+    </View>
+  );
+};
