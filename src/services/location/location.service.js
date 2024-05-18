ba33271e3625564023a369b605656fcecfbb4564
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-18 00:12:39 +0000

    Setting Up location with fake api

---
+++ src/services/location/location.service.js
@@ -0,0 +1,22 @@
+import camelize from "camelize";
+
+import { locations } from "./location.mock";
+import { result } from "lodash";
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((resolve, reject) => {
+    const locationMock = locations[searchTerm];
+    if (!locationMock) {
+      reject("not found");
+    }
+    resolve(locationMock);
+  });
+};
+
+export const locationTransform = () => {
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lat, lng } = geometry.location;
+
+  return { lat, lng };
+};
